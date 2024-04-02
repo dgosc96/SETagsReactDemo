14c0c095547a454933799994754350648aa23f5c
dgosc96 <16181821+dgosc96@users.noreply.github.com>
2024-04-02 02:16:52 +0200

    Install and configure Storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,36 @@
+import type { Preview } from '@storybook/react';
+
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+
+import theme from '../src/theme';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+
+  decorators: [
+    withThemeFromJSXProvider({
+      GlobalStyles: CssBaseline,
+      Provider: ThemeProvider,
+      themes: {
+        // Provide your custom themes here
+        dark: theme,
+      },
+      defaultTheme: 'dark',
+    }),
+  ],
+};
+
+export default preview;
