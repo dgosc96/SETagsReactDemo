5b30f555a98b1bf4a83334538dd66cfe464815ca
dgosc96 <16181821+dgosc96@users.noreply.github.com>
2024-04-04 07:51:28 +0200

    improve code for setting page count

--- src/shared/queries/useTagsQuery.ts
+++ src/shared/queries/useTagsQuery.ts
@@ -2,18 +2,25 @@ import { useQuery } from '@tanstack/react-query';
 import { useTagsFetchParamsContext } from '../context/TagsContext';
 
 import { calculateTotalPages } from '../utils/calculateTotalPages';
+import { useEffect } from 'react';
 
 export const useTagsQuery = () => {
   const { fetchParams, pagesize, fetchFn, setPageCount } =
     useTagsFetchParamsContext();
-  return useQuery({
+
+  const query = useQuery({
     queryKey: ['tags', fetchParams],
     queryFn: () => fetchFn(fetchParams),
-    select: (data) => {
-      setPageCount(calculateTotalPages(pagesize, data.total));
-      return data;
-    },
     gcTime: 1000 * 60 * 10,
     staleTime: 1000 * 60 * 5,
   });
+
+  useEffect(() => {
+    if (query.data) {
+      setPageCount(calculateTotalPages(pagesize, query.data.total));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query.data]);
+
+  return query;
 };
